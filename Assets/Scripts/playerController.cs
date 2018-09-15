c6c0007bed4bdbca8a9dceca9a27e01786ee75e2
Nick <nickconway175@gmail.com>
2018-09-14 23:32:12 -0400

    Attempt to upload files

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    //variables
+    public float mSpeed;
+    // Use this for initialization
+    void Start () {
+        mSpeed = .05f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(mSpeed * Input.GetAxisRaw("Horizontal"), mSpeed * Input.GetAxisRaw("Vertical"), 0f);
+
+        //original: transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+    }
+}
