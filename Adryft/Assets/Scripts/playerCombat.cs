d0045e4fa8cc958a2faa5b9ff15e0a62ed4e9473
escolasticokid <escolasticokid@gmail.com>
2018-09-26 16:04:09 -0400

    Added script for combat

---
+++ Adryft/Assets/Scripts/playerCombat.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class playerCombat : MonoBehaviour {
+    /*
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeBtwAttack <= 0)
+        {
+            //then you can attack
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+	}
+
+    void OnDrawGizmoSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+    */
+}
