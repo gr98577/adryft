d3485634a249a4e4664f148752cd2e4c181cbf71
liamupton1 <liamupton1@gmail.com>
2019-01-09 14:36:03 -0500

    Added a pause Menu

---
+++ Adryft/Assets/PauseScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public static bool gameIsPaused;
+    [SerializeField]
+    public GameObject menuUI;
+
+
+	// Use this for initialization
+	void Start () {
+        pauseResume();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused) { pauseResume(); }
+            else { pauseOpen(); }
+        }
+	}
+
+    private void pauseOpen()
+    {
+        menuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        // Have to go in a make it manually disable scripts that continue even when timescale is 0
+        // You can check if the game is paused from any in game script with " if(pauseScript.gameIsPaused) "
+    }
+
+    public void pauseResume()
+    {
+        menuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void pauseRestart()
+    {
+        pauseResume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void pauseQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
