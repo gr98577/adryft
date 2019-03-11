0a620bf58ea03fd5fe5c00aa388ac69078c86213
Emmanuel Olaleye <gr98577@umbc.edu>
2019-03-10 23:31:35 -0400

    Added split sprites for player and slug; started work on implementing player movement.

--- Adryft/Assets/Scripts/Game/player/playerAnimScript.cs
+++ Adryft/Assets/Scripts/Game/player/playerAnimScript.cs
@@ -7,10 +7,27 @@ public class playerAnimScript : MonoBehaviour {
     private SpriteRenderer sr;
     private Sprite front;
     private Sprite back;
+    private Sprite[] fWalk = new Sprite[3], bWalk = new Sprite[3], lWalk = new Sprite[3], rWalk = new Sprite[3];
 
 	// Use this for initialization
 	void Start () {
         sr = GetComponent<SpriteRenderer>();
+        fWalk[0] = Resources.Load<Sprite>("PlayerAnim/up/1");
+        fWalk[1] = Resources.Load<Sprite>("PlayerAnim/up/2");
+        fWalk[2] = Resources.Load<Sprite>("PlayerAnim/up/3");
+
+        bWalk[0] = Resources.Load<Sprite>("playerAnim/down/1");
+        bWalk[1] = Resources.Load<Sprite>("playerAnim/down/2");
+        bWalk[2] = Resources.Load<Sprite>("playerAnim/down/3");
+
+        lWalk[0] = Resources.Load<Sprite>("playerAnim/left/1");
+        lWalk[1] = Resources.Load<Sprite>("playerAnim/left/2");
+        lWalk[2] = Resources.Load<Sprite>("playerAnim/left/3");
+
+        rWalk[0] = Resources.Load<Sprite>("playerAnim/right/1");
+        rWalk[1] = Resources.Load<Sprite>("playerAnim/right/2");
+        rWalk[2] = Resources.Load<Sprite>("playerAnim/right/3");
+
         front = Resources.Load<Sprite>("PlayerAnim/PlayerFront");
         back = Resources.Load<Sprite>("PlayerAnim/PlayerBack");
     }
@@ -19,9 +36,9 @@ public class playerAnimScript : MonoBehaviour {
 	void LateUpdate () {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
+            
         // Sets its location to the player's and put itself on top of or under the player depending on mouse location
-        if (mousePosition.y >= transform.position.y)
+       /* if (mousePosition.y >= transform.position.y)
         {
             sr.sprite = back;
         }
@@ -29,5 +46,10 @@ public class playerAnimScript : MonoBehaviour {
         {
             sr.sprite = front;
         }
+        */
+        
+        if(mousePosition.y )
+
+
     }
 }
